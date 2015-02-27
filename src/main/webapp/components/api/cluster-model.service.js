6740715e32ec344338d0ecef700da5d5e5fc6147
Tomas Sykora <tsykora@redhat.com>
2015-02-27 17:08:06 +0100

    Added checks for cluster and cache availability

--- src/main/webapp/components/api/cluster-model.service.js
+++ src/main/webapp/components/api/cluster-model.service.js
@@ -14,6 +14,8 @@ angular.module('managementConsole.api')
                 this.lastRefresh = null;
                 this.caches = []; // desired to have caches in an array so we can filter out through their names
                 this.cachesNameMap = {}; // hashMap for fast Cache object referencing by cache name
+                this.cachesByServerName = {}; // serverName -> [caches]
+                this.clusterAvailability = this.refreshClusterAvailability();
             };
 
             Cluster.prototype.getModelController = function () {
@@ -26,28 +28,55 @@ angular.module('managementConsole.api')
 
             Cluster.prototype.refresh = function () {
                 return this.modelController.readResource(this.getResourcePath(), false, false).then(function (response) {
+                    this.refreshClusterAvailability();
                     this.lastRefresh = new Date();
                     this.caches = [];
                     var cachePromises = [];
-                    var cacheTypes = ['local-cache', 'distributed-cache', 'replicated-cache', 'invalidation-cache'];
-                    for (var i = 0; i < cacheTypes.length; i++) {
-                        var typedCaches = response[cacheTypes[i]];
-                        if (typedCaches !== undefined) {
-                            for (var name in typedCaches) {
-                                if (name !== undefined) {
-                                    var cache = new CacheModel(name, cacheTypes[i], this);
-                                    this.caches.push(cache);
-                                    this.cachesNameMap[name] = cache;
-                                    cachePromises.push(cache.refresh());
+                    var nodes = this.getNodes();
+
+                    for (var y = 0; y < nodes.length; y++) {
+                        var tmpCachesForOneServer = [];
+                        var cacheTypes = ['local-cache', 'distributed-cache', 'replicated-cache', 'invalidation-cache'];
+                        for (var i = 0; i < cacheTypes.length; i++) {
+                            var typedCaches = response[cacheTypes[i]];
+                            if (typedCaches !== undefined) {
+                                for (var name in typedCaches) {
+                                    if (name !== undefined) {
+
+                                        // TODO: All caches on all nodes. Do it on request?
+                                        // TODO: Possible bottleneck for big deployments!
+                                        var cache = new CacheModel(name, cacheTypes[i], this.domain, this, nodes[y].server);
+                                        this.caches.push(cache);
+                                        this.cachesNameMap[name] = cache;
+                                        tmpCachesForOneServer.push(cache);
+                                        cachePromises.push(cache.refresh());
+                                    }
                                 }
                             }
                         }
+
+                        this.cachesByServerName[nodes[y].server] = tmpCachesForOneServer;
+                        tmpCachesForOneServer = [];
                     }
                     return $q.all(cachePromises);
                 }.bind(this));
             };
 
-            Cluster.prototype.getAvailability = function () {};
+            Cluster.prototype.refreshClusterAvailability = function () {
+
+                // Temporary: we are checking cluster availability on the first server
+                // Question: is here any was how to check cluster availability globally?
+                var resourcePathCacheContainer = this.domain.getFistServerResourceRuntimePath()
+                        .concat('subsystem', 'infinispan', 'cache-container', this.name);
+
+                var promise = this.modelController.readAttribute(resourcePathCacheContainer, 'cluster-availability');
+                promise.then(function(response) {
+                    this.clusterAvailability = response;
+                }.bind(this),
+                function(error) {
+                    console.log('Could not get cluster availability: ' + error);
+                }.bind(this));
+            };
 
             Cluster.prototype.getNodes = function () {
                 return this.domain.getNodes();
@@ -65,6 +94,10 @@ angular.module('managementConsole.api')
              */
             Cluster.prototype.getCachesNameMap = function () {
                 return this.cachesNameMap;
+            };
+
+            Cluster.prototype.getCachesByServerName = function (serverName) {
+                return this.cachesByServerName[serverName];
             };
 
             return Cluster;

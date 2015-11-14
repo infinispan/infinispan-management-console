0d14323bc060ca6117fe303d01247ca0cad9c2af
vblagoje <vblagoje@redhat.com>
2015-11-14 07:44:53 -0500

    Add refresh button to all relevant pages

--- src/main/webapp/cache-nodes/cache-nodes.controller.js
+++ src/main/webapp/cache-nodes/cache-nodes.controller.js
@@ -1,28 +1,33 @@
 'use strict';
 
 angular.module('managementConsole')
-    .controller('CacheNodesCtrl', [
+  .controller('CacheNodesCtrl', [
     '$scope',
     '$stateParams',
     '$state',
     'modelController',
     'utils',
     function ($scope, $stateParams, $state, modelController, utils) {
-            if (!$stateParams.clusterName && !$stateParams.cacheName) {
-                $state.go('error404');
-            }
-            var server = modelController.getServer();
-            var clusters = server.getClusters();
-            $scope.currentCluster = server.getCluster(clusters, $stateParams.clusterName);
-            $scope.caches = $scope.currentCluster.getCaches();
-            $scope.currentCache = $scope.caches[$stateParams.cacheName];
-            $scope.currentCache.refresh();
-            $scope.currentCacheStats = {};
-            var p = server.fetchCacheStats($scope.currentCluster, $scope.currentCache);
-            p.then(function (response) {
-                $scope.currentCacheStats.nodeStats = response;
-            });
-            $scope.currentCacheAvailability = function () {
-              return utils.isNotNullOrUndefined($scope.currentCluster) && $scope.currentCluster.isAvailable();
-            };
+      if (!$stateParams.clusterName && !$stateParams.cacheName) {
+        $state.go('error404');
+      }
+      $scope.currentCacheAvailability = function () {
+        return utils.isNotNullOrUndefined($scope.currentCluster) && $scope.currentCluster.isAvailable();
+      };
+
+      $scope.refresh = function () {
+        var server = modelController.getServer();
+        var clusters = server.getClusters();
+        $scope.currentCluster = server.getCluster(clusters, $stateParams.clusterName);
+        $scope.caches = $scope.currentCluster.getCaches();
+        $scope.currentCache = $scope.caches[$stateParams.cacheName];
+        $scope.currentCache.refresh();
+        $scope.currentCacheStats = {};
+        var p = server.fetchCacheStats($scope.currentCluster, $scope.currentCache);
+        p.then(function (response) {
+          $scope.currentCacheStats.nodeStats = response;
+        });
+      };
+
+      $scope.refresh();
     }]);

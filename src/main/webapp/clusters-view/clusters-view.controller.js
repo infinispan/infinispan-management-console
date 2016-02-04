47b946c3f5cb1b432af85838e6fdd30f0665d946
pzapata <pzapataf@redhat.com>
2016-02-04 16:44:55 +0100

    ISPN-5809 - View of cluster events

--- src/main/webapp/clusters-view/clusters-view.controller.js
+++ src/main/webapp/clusters-view/clusters-view.controller.js
@@ -12,8 +12,77 @@ angular.module('managementConsole')
             $scope.group = modelController.getServer().getServerGroupName();
             $scope.stack = modelController.getServer().getFirstServer().getDefaultStack();
             $scope.relays = {};
+
             //endpoints for each cache container
             $scope.endpoints = [];
+
+            $scope.offlineSites = {};
+            $scope.onlineSites  = {};
+            $scope.mixedSites   = {};
+
+            //
+            // Updates the map of remote site status
+            //
+            $scope.refreshRemoteSitesStatus = function(cluster) {
+              var resourcePathCacheContainer = cluster.domain.getFirstServer().getResourcePath()
+                 .concat('subsystem', 'datagrid-infinispan', 'cache-container', cluster.name);
+
+                // Refresh list of offline sites
+                cluster.modelController.readAttribute(resourcePathCacheContainer, 'sites-offline').then(
+                  function (response){
+                      if( response != null && response.constructor === Array) {
+                        $scope.offlineSites[cluster.name] = response
+                      } else {
+                        $scope.offlineSites[cluster.name] = [];
+                      }
+                  }
+                );
+
+                // Refresh list of online sites
+                cluster.modelController.readAttribute(resourcePathCacheContainer, 'sites-online').then(
+                  function (response){
+                      if( response != null && response.constructor === Array) {
+                        $scope.onlineSites[cluster.name] = response
+                      } else {
+                        $scope.onlineSites[cluster.name] = [];
+                      }
+                  }
+                );
+
+                // Refresh list of mixed sites
+                cluster.modelController.readAttribute(resourcePathCacheContainer, 'sites-mixed').then(
+                  function (response){
+                    if( response != null && response.constructor === Array) {
+                      $scope.mixedSites[cluster.name] = response
+                    } else {
+                      $scope.mixedSites[cluster.name] = [];
+                    }
+                  }
+               );
+            };
+
+
+            //
+            // Loads latest grid events
+            //
+            $scope.refreshGridEvents = function() {
+                $scope.gridEvents = [];
+                angular.forEach($scope.clusters, function(cluster){
+                  $scope.refreshClusterEvents(cluster, 10);
+                });
+            }
+
+            $scope.refreshClusterEvents = function(cluster, maxLines) {
+                    cluster.fetchClusterEvents(maxLines).then(
+                       function (response) {
+                          angular.forEach(response, function ( event ) {
+                            $scope.gridEvents.push(event);
+                            });
+                       }
+                    );
+               }
+
+
             angular.forEach($scope.clusters, function(cluster){
               var relays = cluster.getRelays();
               if (utils.isNonEmptyArray(relays)){
@@ -26,4 +95,7 @@ angular.module('managementConsole')
               }
               $scope.endpoints.push(cluster.getEndpoints());
             });
+
+            // Refresh grid events
+            $scope.refreshGridEvents();
   }]);

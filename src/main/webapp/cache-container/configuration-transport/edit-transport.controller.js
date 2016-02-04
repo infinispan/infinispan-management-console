e8b2c17e4369f7b9bc431763751f3e411284a447
vblagoje <vblagoje@redhat.com>
2016-02-04 11:04:05 -0500

    ISPN-5813 View of cache container configuration (transport uses require restart)

--- src/main/webapp/cache-container/configuration-transport/edit-transport.controller.js
+++ src/main/webapp/cache-container/configuration-transport/edit-transport.controller.js
@@ -6,20 +6,75 @@ angular.module('managementConsole')
     '$state',
     '$stateParams',
     'utils',
+    '$modal',
     'modelController',
     'cacheContainerConfigurationService',
-    function ($scope, $state, $stateParams, utils, modelController, cacheContainerConfigurationService) {
+    'clusterNodesService',
+    function ($scope, $state, $stateParams, utils, $modal, modelController, cacheContainerConfigurationService, clusterNodesService) {
       if (!$stateParams.clusterName) {
         $state.go('error404');
       }
 
+      var RequiresRestartModalInstanceCtrl = function ($scope, $modalInstance) {
+
+        $scope.ok = function () {
+          $modalInstance.close(true);
+        };
+
+        $scope.cancel = function () {
+          $modalInstance.dismiss();
+        };
+      };
+
       $scope.currentCluster = modelController.getServer().getClusterByName($stateParams.clusterName);
       $scope.transport = $scope.currentCluster.getTransportConfiguration();
       $scope.metadata = $scope.currentCluster.getMetadata().children['transport']['model-description'];
+      $scope.configurationHandle = {};
 
-      $scope.save = function(){
+      $scope.requiresRestart = function (){
+        return $scope.configurationHandle.requiresRestart();
+      };
+
+      $scope.hasDirtyFields = function (){
+        return $scope.configurationHandle.isAnyFieldModified();
+      };
+
+      $scope.cleanMetadata = function (){
+        $scope.configurationHandle.cleanMetadata();
+      };
+
+      $scope.saveHelper = function(restart){
         var address = $scope.currentCluster.getResourcePath().concat('transport','TRANSPORT');
-        cacheContainerConfigurationService.saveTransport(address, $scope.transport);
+        var promise = cacheContainerConfigurationService.saveTransport(address, $scope.transport);
+        if (restart){
+          promise.then(function (){
+            clusterNodesService.restartCluster();
+          });
+        }
+      };
+
+      $scope.save = function () {
+        if ($scope.hasDirtyFields()) {
+          var rr = $scope.requiresRestart();
+          if (rr) {
+            var dialog = $modal.open({
+              templateUrl: 'components/dialogs/requires-restart.html',
+              controller: RequiresRestartModalInstanceCtrl,
+              scope: $scope
+            });
+
+            dialog.result.then(function (requiresRestart) {
+              if (requiresRestart) {
+                $scope.saveHelper(true);
+              } else {
+                $scope.saveHelper(false);
+              }
+            });
+          } else {
+            $scope.saveHelper(false);
+          }
+        }
+        $scope.cleanMetadata();
       };
 
       $scope.cancel = function(){

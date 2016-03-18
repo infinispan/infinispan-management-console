a6696f66bece6efb3a571017d6f2d81bd5635465
vblagoje <vblagoje@redhat.com>
2016-03-18 12:30:51 -0400

    Move cache ops into cache service

--- src/main/webapp/components/api/cache-model.service.js
+++ src/main/webapp/components/api/cache-model.service.js
@@ -91,58 +91,13 @@ angular.module('managementConsole.api')
             };
 
             Cache.prototype.isTransactional = function () {
-              return utils.isNotNullOrUndefined(this.configuration.transaction) && (this.configuration.transaction.TRANSACTION.mode !== 'NONE');
+              return utils.isNotNullOrUndefined(this.configuration.transaction) &&
+                (this.configuration.transaction.TRANSACTION.mode !== 'NONE');
             };
 
             Cache.prototype.isSecured = function () {
-              return utils.isNotNullOrUndefined(this.configuration.security) && utils.isNotNullOrUndefined(this.configuration.security.SECURITY.authorization);
-            };
-
-            Cache.prototype.start = function () {
-              return this.executeOp('start-cache');
-            };
-
-            Cache.prototype.stop = function () {
-              return this.executeOp('stop-cache');
-            };
-
-            Cache.prototype.enable = function () {
-              var profile = this.getCluster().getProfile();
-              var endpointAddress = profile.getResourcePath().concat('subsystem','datagrid-infinispan-endpoint');
-              var op = {
-                'operation': 'unignore-cache-all-endpoints',
-                'cache-names':[this.name],
-                'address': endpointAddress
-              };
-              return this.getModelController().execute(op);
-            };
-
-            Cache.prototype.disable = function () {
-              var profile = this.getCluster().getProfile();
-              var endpointAddress = profile.getResourcePath().concat('subsystem','datagrid-infinispan-endpoint');
-              var op = {
-                'operation': 'ignore-cache-all-endpoints',
-                'cache-names':[this.name],
-                'address': endpointAddress
-              };
-              return this.getModelController().execute(op);
-            };
-
-            Cache.prototype.flush = function () {
-              return this.executeOp('flush-cache');
-            };
-
-            Cache.prototype.resetStats = function () {
-              return this.executeOp('reset-statistics');
-            };
-
-            Cache.prototype.executeOp = function (operationName){
-              var address = this.getResourcePath();
-              var op = {
-                'operation': operationName,
-                'address': address
-              };
-              return this.getModelController().execute(op);
+              return utils.isNotNullOrUndefined(this.configuration.security) &&
+                utils.isNotNullOrUndefined(this.configuration.security.SECURITY.authorization);
             };
 
             return Cache;

c2af89355aa21289e198b8ad2656b8e54755fb47
vblagoje <vblagoje@redhat.com>
2016-08-18 04:49:24 -0400

    ISPN-6807 Customization of console features by configuration

---
+++ src/main/webapp/env.js
@@ -0,0 +1,8 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  window.__env.infinispan = true;
+  window.__env.jdg = false;
+  window.__env.brandImage = 'assets/images/brand.svg';
+  window.__env.brandName = 'Infinispan Server Management Console';
+}(this));

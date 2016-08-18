e2cf3033c403298063c4682669f088a47b99f039
Ryan Emerson <remerson@redhat.com>
2016-08-18 09:24:17 +0100

    Old console code removed.

--- src/main/webapp/clusters-view/test.controller.ts
+++
@@ -1,7 +0,0 @@
-import {App} from "../App";
-
-// TODO when we re-work controller as Typescript objects we should not register to module, but just export class
-const module: ng.IModule = App.module("managementConsole", []);
-module.controller("TestCtrl", function () {
-
-});
--- src/main/webapp/constants.ts
+++
@@ -1,4 +0,0 @@
-export module Constants {
-  export const SRC_DIR:string = "src/main/webapp";
-  export const ASSETS_DIR:string = "main/assets";
-}

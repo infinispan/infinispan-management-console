552e3ab1644d9e61c9e38471b33ea3c5140b7f90
Ryan Emerson <remerson@redhat.com>
2017-07-14 10:38:40 +0100

    ISPN-8066 Json meta files not included in dist build.

--- gulpfile.js
+++ gulpfile.js
@@ -30,13 +30,14 @@ gulp.task('clean', ['clean:src', 'clean:dist']);
 gulp.task('less', require('./tasks/less')(gulp, config));
 gulp.task('fonts', require('./tasks/fonts')(gulp, config));
 gulp.task('html', require('./tasks/html')(gulp, config));
+gulp.task('json', require('./tasks/json')(gulp, config));
 gulp.task('assets', require('./tasks/assets')(gulp, config));
 
 gulp.task('compile:src', ['clean:src'], require('./tasks/compile')(gulp, config.srcDir));
 gulp.task('compile', ['compile:src']);
 
 gulp.task('serve:dist', ['build:dist'], require('./tasks/server')(gulp, config.distDir, false, true));
-gulp.task('serve', ['compile:src', 'less', 'fonts', 'html', 'assets'],
+gulp.task('serve', ['compile:src', 'less', 'fonts', 'html', 'json', 'assets'],
     require('./tasks/server')(gulp, config.srcDir, config.watchDir, true, config.projectDir, config));
 
 gulp.task('check:eslint', require('./tasks/check-eslint')(gulp, config));
@@ -47,7 +48,7 @@ gulp.task('check', require('./tasks/check')());
 gulp.task('ng:directives', ['compile:src'], require('./tasks/ng-directives')(gulp, config));
 gulp.task('ng:annotate', ['ng:directives'], require('./tasks/ng-annotate')(gulp, config));
 
-gulp.task('build:dist', ['ng:annotate', 'less', 'fonts', 'html', 'assets'], require('./tasks/build-dist')(gulp, config));
+gulp.task('build:dist', ['ng:annotate', 'less', 'fonts', 'html', 'json', 'assets'], require('./tasks/build-dist')(gulp, config));
 gulp.task('build', require('./tasks/build')());
 
 gulp.task('default', require('./tasks/default')());

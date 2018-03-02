cce7290928b4a7edc493d35e1cb4214c084e0cb9
vblagoje <vblagoje@redhat.com>
2018-03-02 16:52:53 +0100

    ISPN-8827 Disable browser cache for some resources in Admin Console

--- tasks/html.js
+++ tasks/html.js
@@ -1,7 +1,7 @@
 module.exports = (gulp, config) => () => {
   const path = require('path');
 
-  return gulp.src([path.join(config.srcDir, '/**/*.html'), '!src/index.html'])
+  return gulp.src([path.join(config.srcDir, '/**/*.html'), '!src/index.html', '!src/index_tmp.html'])
       .pipe(gulp.dest(config.distDir));
 
 };

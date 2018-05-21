d6ab1b6e0edcb461fd1ef6d35f30e9a792b6c856
vblagoje <vblagoje@redhat.com>
2018-05-21 15:28:20 +0200

    ISPN-7085 Add Codemirror editor for editing scripts

--- src/module/cache-container/config/tasks/ScriptModalCtrl.ts
+++ src/module/cache-container/config/tasks/ScriptModalCtrl.ts
@@ -1,33 +1,56 @@
+import CodeMirror = require("codemirror");
+import {EditorConfiguration} from "codemirror";
 import {IStateService} from "angular-ui-router";
 import {ICacheContainer} from "../../../../services/container/ICacheContainer";
 import {ContainerConfigService} from "../../../../services/container-config/ContainerConfigService";
 import {IDeployPageScope} from "../../../../services/container-config/IDeployPageScope";
 import {ITask} from "../../../../services/task/ITask";
 import {TasksCtrl} from "./TasksCtrl";
+import IModalServiceInstance = angular.ui.bootstrap.IModalServiceInstance;
 
 export class ScriptModalCtrl {
-  static $inject: string[] = ["$state", "$scope", "container", "containerConfigService", "task", "editing", "parentController"];
+  static $inject: string[] = ["$uibModalInstance", "$state", "$scope", "container", "containerConfigService", "task", "editing", "parentController"];
 
-  constructor(private $state: IStateService,
+  errorExecuting: boolean = false;
+  errorDescription: string = "";
+  editor: CodeMirror.Editor;
+
+  constructor(private $uibModalInstance: IModalServiceInstance,
+              private $state: IStateService,
               private $scope: IDeployPageScope,
               private container: ICacheContainer,
               private containerConfigService: ContainerConfigService,
               private task: ITask,
               private editing: boolean,
               private parentController: TasksCtrl) {
+
+    // Once modal is rendered, "dress up" modal textarea with CodeMirror editor
+    $uibModalInstance.rendered.then(() => {
+      let area: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("container-config.tasks.edit.body.text");
+      let editorConfig: EditorConfiguration = <EditorConfiguration> {
+        lineNumbers: true,
+        mode: "javascript"
+      };
+      this.editor = CodeMirror.fromTextArea(area, editorConfig);
+    });
   }
 
   // Task creation
   createScript(): void {
-    // In edit mode, we allow the user to edit the script directly
-    let realBody: string = this.editing ? this.task.body : this.buildBody();
+    let realBody: string = this.buildBody(this.editing);
 
-    this.containerConfigService.deployScript(this.container, this.task, realBody).then((response: any) => {
-      this.parentController.reloadScripts();
-    }).catch((e) => {
-        this.parentController.openErrorModal(e.toString());
-      }
-    );
+    if (this.editor.getValue().length === 0) {
+      this.errorExecuting = true;
+      this.errorDescription = "Can not create empty script!";
+    } else {
+      this.containerConfigService.deployScript(this.container, this.task, realBody).then((response: any) => {
+        this.$uibModalInstance.close(true);
+        this.parentController.reloadScripts();
+      }).catch((e) => {
+          this.parentController.openErrorModal(e.toString());
+        }
+      );
+    }
   };
 
   removeScript(): void {
@@ -39,8 +62,7 @@ export class ScriptModalCtrl {
     );
   }
 
-  private buildBody(): string {
-
+  private buildBody(editing: boolean): string {
     let body: string = "// name=" + this.task.name + ", language=" + this.task.language + "\n";
     body += "// mode=" + this.task.mode + "\n";
     if (this.task.parameters && this.task.parameters.length > 0) {
@@ -49,10 +71,13 @@ export class ScriptModalCtrl {
     if (this.task.role && this.task.role.length > 0) {
       body += "// role=" + this.task.role + "\n";
     }
-
     body += "\n";
-    body += this.task.body;
 
+    if (!editing) {
+      body += this.editor.getValue();
+    } else {
+      body = this.editor.getValue();
+    }
     return body;
   }
 }
